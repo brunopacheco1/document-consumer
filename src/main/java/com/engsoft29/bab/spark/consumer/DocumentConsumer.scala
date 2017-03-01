2a357e8ca64c961c51c851258edc403023c2edc8
Bruno Pacheco <brunopacheco1@yahoo.com>
2017-03-01 13:42:58 -0300

    Processando documento via kafka e spark streaming e ajuste no pagerank.

--- src/main/java/com/engsoft29/bab/spark/consumer/DocumentConsumer.scala
+++ src/main/java/com/engsoft29/bab/spark/consumer/DocumentConsumer.scala
@@ -1,19 +1,44 @@
 package com.engsoft29.bab.spark.consumer
 
-import org.apache.kafka.clients.consumer.ConsumerRecord
+import java.security.MessageDigest
+
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
-import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.Seconds
 import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.elasticsearch.spark.rdd.EsSpark
+
+import spray.json._
+import DefaultJsonProtocol._
 
 object DocumentConsumer {
+
+  private def buildResult(jsonStr: String): Map[String, Any] = {
+    case class Model(url: String, document: String, urls: Array[String])
+    implicit val modelFormat = jsonFormat3(Model)
+    val json = jsonStr.parseJson.convertTo[Model]
+    val summary = json.document.subSequence(0, Math.min(json.document.length(), 150))
+
+    val pagerank = 0d
+
+    Map("id" -> hash(json.url), "url" -> json.url, "summary" -> summary, "children" -> json.urls.map(el => hash(el)), "pagerank" -> pagerank)
+  }
+
+  private def hash(inputStr: String): String = {
+    val md: MessageDigest = MessageDigest.getInstance("MD5")
+    md.digest(inputStr.getBytes()).map(0xFF & _).map { "%02x".format(_) }.foldLeft("") { _ + _ }
+  }
+
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setAppName("DocumentConsumer").setMaster("local[*]")
 
-    val ssc = new StreamingContext(sparkConf, Seconds(60))
+    val sc = new SparkContext(sparkConf)
+
+    val ssc = new StreamingContext(sc, Seconds(1))
 
     val kafkaParams = Map[String, Object](
       "bootstrap.servers" -> "localhost:9092",
@@ -29,6 +54,15 @@ object DocumentConsumer {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams))
 
-    stream.map(record => (record.key, record.value))
+    val result = stream.map(record => (record.key, record.value))
+
+    result.foreachRDD(rdd => {
+      val result = rdd.map(el => buildResult(el._2))
+
+      EsSpark.saveToEs(result, "documents/document", Map("es.mapping.id" -> "id"))
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
--- src/main/java/com/engsoft29/bab/spark/consumer/PageRanking.scala
+++ src/main/java/com/engsoft29/bab/spark/consumer/PageRanking.scala
@@ -1,20 +1,22 @@
 package com.engsoft29.bab.spark.consumer
 
 import scala.collection.mutable.Buffer
+import scala.util.hashing.MurmurHash3
 
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
+import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.VertexId
 import org.apache.spark.rdd.RDD
-import org.apache.spark.graphx._
-import org.elasticsearch.spark._
-import scala.util.hashing.MurmurHash3
+import org.elasticsearch.spark.rdd.EsSpark
+import org.elasticsearch.spark.sparkContextFunctions
 
 object PageRanking {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext(new SparkConf().setAppName("BaBPagerank").setMaster("local[*]"))
+    val sc = new SparkContext(new SparkConf().set("es.write.operation","upsert").setAppName("BaBPagerank").setMaster("local[*]"))
 
     val documents = sc.esRDD("documents/document", Map[String, String]("es.read.field.include" -> "id,children")).map(kv => kv._2)
-    val filteredDocuments = documents.filter(value => value("children").asInstanceOf[Buffer[String]].length > 0).map(val2 => val2("children").asInstanceOf[Buffer[String]].map(child => (val2("id").asInstanceOf[String], child))).flatMap(identity)
+    val filteredDocuments = documents.filter(value => value.contains("children") && value("children").asInstanceOf[Buffer[String]].length > 0).map(val2 => val2("children").asInstanceOf[Buffer[String]].map(child => (val2("id").asInstanceOf[String], child))).flatMap(identity)
 
     val edges: RDD[(VertexId, VertexId)] = filteredDocuments.map(el => (MurmurHash3.stringHash(el._1), MurmurHash3.stringHash(el._2)))
 
@@ -24,9 +26,11 @@ object PageRanking {
 
     val vertices = filteredDocuments.map(el => (MurmurHash3.stringHash(el._1).asInstanceOf[org.apache.spark.graphx.VertexId], el._1)) ++ filteredDocuments.map(el => (MurmurHash3.stringHash(el._2).asInstanceOf[org.apache.spark.graphx.VertexId], el._2))
 
-    val rankByVertices = vertices.join(ranks).map(el => el._2)
+    val rankByVertices = vertices.join(ranks).map(el => Map("id" -> el._2._1, "pagerank" -> el._2._2))
     
-    rankByVertices.collect().foreach(println)
+    EsSpark.saveToEs(rankByVertices, "documents/document", Map("es.mapping.id" -> "id"))
+    
+//    rankByVertices.collect().foreach(println)
 
     /*val documentRanked = documents.join(rankByVertices).map(el => el._2).map(el => el._1 ++ Map("pagerank" -> el._2))
 
