bd71f6ae2eccfe912ca93ac7cf2080b442dc712e
Bruno Pacheco <brunopacheco1@yahoo.com>
2017-03-11 11:08:39 -0300

    Ajustes no pagerank.

--- src/main/java/com/engsoft29/bab/spark/consumer/PageRanking.scala
+++ src/main/java/com/engsoft29/bab/spark/consumer/PageRanking.scala
@@ -15,7 +15,10 @@ object PageRanking {
   def main(args: Array[String]): Unit = {
     val sc = new SparkContext(new SparkConf().set("es.write.operation","upsert").setAppName("BaBPagerank").setMaster("local[*]"))
 
-    val documents = sc.esRDD("documents/document", Map[String, String]("es.read.field.include" -> "id,children")).map(kv => kv._2)
+    val documents = sc.esRDD("documents/document", Map[String, String]("es.read.field.include" -> "id,children,pagerank")).map(kv => kv._2)
+    
+    println("Total de documentos: " + documents.count())
+    
     val filteredDocuments = documents.filter(value => value.contains("children") && value("children").asInstanceOf[Buffer[String]].length > 0).map(val2 => val2("children").asInstanceOf[Buffer[String]].map(child => (val2("id").asInstanceOf[String], child))).flatMap(identity)
 
     val edges: RDD[(VertexId, VertexId)] = filteredDocuments.map(el => (MurmurHash3.stringHash(el._1), MurmurHash3.stringHash(el._2)))
@@ -24,9 +27,11 @@ object PageRanking {
 
     val ranks = graph.pageRank(0.0001).vertices
 
-    val vertices = filteredDocuments.map(el => (MurmurHash3.stringHash(el._1).asInstanceOf[org.apache.spark.graphx.VertexId], el._1)) ++ filteredDocuments.map(el => (MurmurHash3.stringHash(el._2).asInstanceOf[org.apache.spark.graphx.VertexId], el._2))
-
-    val rankByVertices = vertices.join(ranks).map(el => Map("id" -> el._2._1, "pagerank" -> el._2._2))
+    val vertices = documents.map(value => (MurmurHash3.stringHash(value("id").toString()).asInstanceOf[org.apache.spark.graphx.VertexId], value("id").toString()))
+    
+    val rankByVertices = vertices.join(ranks).map(el => Map("id" -> el._2._1, "pagerank" -> el._2._2)).filter(value => value("pagerank") != null && value("pagerank").toString.toDouble > 0)
+    
+    println("Total de documentos rankeados: " + rankByVertices.count())
     
     EsSpark.saveToEs(rankByVertices, "documents/document", Map("es.mapping.id" -> "id"))
   }
